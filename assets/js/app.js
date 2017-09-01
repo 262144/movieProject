a4574a22c2743978c60b1eb6bb6892285fffba6f
unknown <gregory.knight@outlook.com>
2017-08-31 21:13:21 -0400

    Using Blakes HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,3 @@
 
 // OMBD api key: 40e9cece
 
-// button click even
-    $("#movie-search").on("click", function(event) {
-      event.preventDefault();
-      console.log("movie search button clicked");
-
-      // Ajax call here
-
-    });
