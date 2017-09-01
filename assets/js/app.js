c083ee2314bd365dcfd866510efd241f8c81962f
unknown <gregory.knight@outlook.com>
2017-08-31 20:37:23 -0400

    button click event, search button id added

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// API Info
+
+// Youtube API key: AIzaSyAulzYOCieayBZYa7roULV60zyAU3B2AtY
+
+// OMBD api key: 40e9cece
+
+// button click even
+    $("#movie-search").on("click", function(event) {
+      event.preventDefault();
+      console.log("movie search button clicked");
+
+      // Ajax call here
+
+    });
